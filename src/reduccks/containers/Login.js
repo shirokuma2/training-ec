4ce96c675d9de7aa6ac26658671839bd30def31e
shirokuma2 <devlop.milano2022@gmail.com>
2022-02-27 00:17:44 +0900

    コンテナーコンポーネント見学

---
+++ src/reduccks/containers/Login.js
@@ -0,0 +1,25 @@
+// functionコンポーネントを利用するためコンテナーコンポーネントは利用しない
+// import LoginClass from "../../templates/LoginClass";
+// import { compose } from "redux";
+// import { connect } from "react-redux";
+// import * as Actions from "../users/operations";
+
+// const mapStateToProps = (state) => {
+//   return {
+//     users: state.users,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     actions: {
+//       signIn() {
+//         dispatch(Actions.signIn());
+//       },
+//     },
+//   };
+// };
+
+// export default compose(connect(mapStateToProps, mapDispatchToProps))(
+//   LoginClass
+// );
